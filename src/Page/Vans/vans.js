e60c72b4225d6e4346651501e5a7a445c9e6f1b4
Yagnik Ladani <y.m.ladnai972@gmail.com>
2023-05-10 18:03:25 +0530

    Final commit of the day and adding filter

--- src/Page/Vans/vans.js
+++ src/Page/Vans/vans.js
@@ -1,34 +1,54 @@
 import './vans.scss'
 import data from '../../data'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 export default function Vans() {
 
-    const card = data.map(d => (
+    const [filter, setFilter] = useState("clear");
+    const filterData = filter === 'clear' ? data : data.filter(item => item.type === filter);
+
+    const card = filterData.map(d => (
         <div key={d.id}>
             <Link to={`${d.id}`}>
-            <img src={d.imageUrl} alt="hello" />
-            <div className='details'>
-                <div className='left'><h2>{d.name}</h2></div>
-                <div className='right'>
-                    <p>${d.price}<span>/day</span></p>
+                <img src={d.imageUrl} alt="hello" />
+                <div className='details'>
+                    <div className='left'><h2>{d.name}</h2></div>
+                    <div className='right'>
+                        <p>${d.price}<span>/day</span></p>
+                    </div>
                 </div>
-            </div>
-            <i className={`type type${d.type}`}>{d.type}</i>
-        </Link>
+                <i className={`type type${d.type}`}>{d.type}</i>
+            </Link>
         </div >
     ))
 
     return (
         <div className='vans'>
             <div className='van-body'>
-            <h1>Explore our van options</h1>
+                <h1>Explore our van options</h1>
                 <div className='filter'>
-                    <button className='filter-button'>simple</button>
-                    <button className='filter-button'>Luxury</button>
-                    <button className='filter-button'>Rugged</button>
-                    <button className='clearfilter'>Clear filters</button>
+                    <button onClick={() => {
+                        setFilter("simple")
+                    }} className={`filter-button ${filter==='simple' ? 'simple' : ''}`}
+                    
+                    
+                    >simple</button>
+                    <button onClick={() => {
+                        setFilter("luxury")
+                    }} className={`filter-button ${filter==='luxury' ? 'luxury' : ''}`}
+                    
+                    
+                    >Luxury</button>
+                    <button onClick={() => {
+                        setFilter("rugged")
+                    }} className={`filter-button ${filter==='rugged' ? 'rugged' : ''}`}
+                    
+                    >Rugged</button>
+                    <button onClick={() => {
+                        setFilter("clear")
+                    }} className='clearfilter'>Clear filters</button>
                 </div>
 
                 <div className='vans-details'>
